acc9beaa0b7b4d049aeab2654de8812dce044bce
Anthony Cruz <anthonyleierlw@gmail.com>
2020-07-10 20:15:41 -0300

    Comunicações entre Componentes

--- src/App.js
+++ src/App.js
@@ -4,23 +4,21 @@ import {SafeAreaView, StyleSheet} from 'react-native';
 // import Primeiro from './components/Primeiro';
 // import CompPadrao, {Comp1, Comp2} from './components/Multi';
 // import MinMax from './components/MinMax';
-import Aleatorio from './components/Aleatorio';
+// import Aleatorio from './components/Aleatorio';
+// import Titulo from './components/Titulo';
+// import Botao from './components/Botao';
+import Contador from './components/Contador';
+import Pai from './components/indireta/Pai';
 
 export default () => (
 	<SafeAreaView style={style.App}>
-		<Aleatorio min={5} max={90}/>
-		<Aleatorio min={5} max={90}/>
-		<Aleatorio min={5} max={90}/>
-		<Aleatorio min={5} max={90}/>
-		<Aleatorio min={5} max={90}/>
-		<Aleatorio min={5} max={90}/>
-		<Aleatorio min={5} max={90}/>
-		<Aleatorio min={5} max={90}/>
-		<Aleatorio min={5} max={90}/>
-		<Aleatorio min={5} max={90}/>
-		{/* <MinMax min="5" max="13" />
-		<MinMax min={15} max={47} /> */}
-		{/* <CompPadrao />
+		<Pai />
+		{/* <Aleatorio min={5} max={90} />
+		<Contador inicial={100} />
+		<Titulo principal="Cadastro Produto" secundario="Tela de Cadastro de Produto"/>
+		<Botao />
+		<MinMax min={15} max={47} />
+		<CompPadrao />
 		<Comp1 />
 		<Comp2 />
 		<Primeiro /> */}
